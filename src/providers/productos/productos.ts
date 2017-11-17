d7580081a54939004fa92ef727ef511b225101d8
CristianG540 <cristianneatproy@gmail.com>
2017-11-17 11:21:14 -0500

    aumento el timeout de la conexion a la base de datos de los productos

--- src/providers/productos/productos.ts
+++ src/providers/productos/productos.ts
@@ -54,6 +54,9 @@ export class ProductosProvider {
         auth: {
           username: "admin",
           password: "admin"
+        },
+        ajax: {
+          timeout: 60000
         }
       });
 

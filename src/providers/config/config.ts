185f1b200c658e0ad19f3da5a1b46629131c3f06
CristianG540 <cristianneatproy@gmail.com>
2017-12-15 09:06:28 -0500

    nuevo release 1.4.3

--- src/providers/config/config.ts
+++ src/providers/config/config.ts
@@ -27,7 +27,7 @@ export class Config {
     });
   }
 
-  static readonly APP_VER: string = "1.4.2";
+  static readonly APP_VER: string = "1.4.3";
   static readonly SUPERLOGIN_URL: string = 'https://www.gatortyres.com:3443';
 
   /* **************************** Cosas de JOSEFA  *************************** */

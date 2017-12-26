e984db96e11976d7c151d5ca2610d42f187a62c6
CristianG540 <cristianneatproy@gmail.com>
2017-12-26 16:48:24 -0500

    nueva clase para el modulo de cartera en el webworker

--- src/assets/js/pouch_replication_worker/src/index.js
+++ src/assets/js/pouch_replication_worker/src/index.js
@@ -97,6 +97,9 @@ class Producto extends DbActions {
 class Cliente extends DbActions {
 }
 
+class Cartera extends DbActions {
+}
+
 self.onmessage = e => {
   let d = e.data
   let localDB = new PouchDB(d.local.name, d.local.options)
@@ -111,6 +114,11 @@ self.onmessage = e => {
     case 'clientes':
       let clientes = new Cliente(localDB, remoteDB)
       clientes.replicate()
+      break
+
+    case 'cartera':
+      let cartera = new Cartera(localDB, remoteDB)
+      cartera.replicate()
       break
 
     default:

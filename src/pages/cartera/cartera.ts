13ced00babbfb88864a27b406bef296a47f44100
CristianG540 <cristianneatproy@gmail.com>
2017-12-26 16:49:20 -0500

    Cambios en el controlador de cartera, para buscar la informacion de cartera por nit del cliente

--- src/pages/cartera/cartera.ts
+++ src/pages/cartera/cartera.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormArray, FormBuilder, Validators  } from "@angular/forms";
 
+//Libs terceros
+import _ from 'lodash';
+import * as moment from 'moment';
+
+//Providers
+import { CarteraProvider } from '../../providers/cartera/cartera';
+import { Config as cg} from '../../providers/config/config';
+
+//Models
+import { Cartera } from "../../providers/cartera/models/cartera_mdl";
+
+
 @IonicPage()
 @Component({
   selector: 'page-cartera',
@@ -10,11 +22,16 @@ import { FormGroup, FormArray, FormBuilder, Validators  } from "@angular/forms";
 export class CarteraPage {
 
   private searchForm: FormGroup;
+  private carteraItems: Cartera[] = [];
+  private totalCliente: number = 0;
+  private loading: boolean = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private carteraServ: CarteraProvider,
+    private util : cg,
   ) {}
 
   ionViewWillLoad() {
@@ -28,6 +45,26 @@ export class CarteraPage {
   }
 
   private onSubmit(): void {
+    let form = JSON.parse(JSON.stringify(this.searchForm.value));
+
+    if(form.cliente){
+      this.totalCliente = 0;
+      this.loading = true;
+      this.carteraServ.searchCartera(form.cliente).then(res=>{
+        this.loading = false;
+        this.carteraItems = _.map(res.rows, (row: any): Cartera => {
+          this.totalCliente += parseInt(row.doc.valor);
+          row.doc.valor = parseInt(row.doc.valor);
+          row.doc.valor_total = parseInt(row.doc.valor_total);
+          row.doc.fecha_emision = moment(row.doc.fecha_emision).format("YYYY-MM-DD");
+          row.doc.fecha_vencimiento = moment(row.doc.fecha_vencimiento).format("YYYY-MM-DD");
+          return row.doc;
+        });
+      }).catch( err => {
+        this.loading = false;
+        this.util.errorHandler(err.message, err);
+      })
+    }
 
   }
 

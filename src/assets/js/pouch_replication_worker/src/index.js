bc042180a1e900294bafc1a30c73167ebea59640
CristianG540 <cristianneatproy@gmail.com>
2018-08-02 15:29:20 -0500

    cambios de version componentes webworker

--- src/assets/js/pouch_replication_worker/src/index.js
+++ src/assets/js/pouch_replication_worker/src/index.js
@@ -32,31 +32,31 @@ class DbActions {
   }
 
   replicate () {
-    this.localDB.replicate.from(this.remoteDB, { batch_size: 50 })
-    .on('change', info => {
-      /**
-       * Esto lo comento por que el enviar muchos mensajes al hilo
-       * principal hace q se bloquee el dom entonces hay q tratat
-       * de enviar la menor cantidad posible de mensajes
-      */
-      // self.postMessage({ event : "change", method: "replicate", info : info })
-    })
-    .on('complete', info => {
-      // Si la primera replicacion se completa con exito sincronizo la db
-      // y de vuelvo la info sobre la sincronizacion
-      this.sync()
-      self.postMessage({ event: 'complete', method: 'replicate', info: info })
-    })
-    .on('error', err => {
-      // Me preguntare a mi mismo en el futuro por que mierda pongo a sincronizar
-      // La base de datos si la primera sincronisacion falla, lo pongo aqui por q
-      // si el usuario cierra la app y la vuelve a iniciar, el evento de initdb
-      // se ejecutaria de nuevo y si por algun motivo no tiene internet entonces
-      // la replicacion nunca se va completar y la base de datos
-      // no se va a sincronizar, por eso lo lanzo de nuevo aqui el sync
-      this.sync()
-      self.postMessage({ event: 'error', method: 'replicate', info: err })
-    })
+    this.localDB.replicate.from(this.remoteDB, { batch_size: 500 })
+      .on('change', info => {
+        /**
+         * Esto lo comento por que el enviar muchos mensajes al hilo
+         * principal hace q se bloquee el dom entonces hay q tratat
+         * de enviar la menor cantidad posible de mensajes
+        */
+        // self.postMessage({ event : "change", method: "replicate", info : info })
+      })
+      .on('complete', info => {
+        // Si la primera replicacion se completa con exito sincronizo la db
+        // y de vuelvo la info sobre la sincronizacion
+        this.sync()
+        self.postMessage({ event: 'complete', method: 'replicate', info: info })
+      })
+      .on('error', err => {
+        // Me preguntare a mi mismo en el futuro por que mierda pongo a sincronizar
+        // La base de datos si la primera sincronisacion falla, lo pongo aqui por q
+        // si el usuario cierra la app y la vuelve a iniciar, el evento de initdb
+        // se ejecutaria de nuevo y si por algun motivo no tiene internet entonces
+        // la replicacion nunca se va completar y la base de datos
+        // no se va a sincronizar, por eso lo lanzo de nuevo aqui el sync
+        this.sync()
+        self.postMessage({ event: 'error', method: 'replicate', info: err })
+      })
   }
 
   sync () {
@@ -65,12 +65,12 @@ class DbActions {
       retry: true
     }
     PouchDB.sync(this.localDB, this.remoteDB, replicationOptions)
-    .on('denied', err => {
-      self.postMessage({ event: 'error', method: 'sync', info: err })
-    })
-    .on('error', err => {
-      self.postMessage({ event: 'error', method: 'sync', info: err })
-    })
+      .on('denied', err => {
+        self.postMessage({ event: 'error', method: 'sync', info: err })
+      })
+      .on('error', err => {
+        self.postMessage({ event: 'error', method: 'sync', info: err })
+      })
     this.reactToChanges()
   }
 
@@ -80,14 +80,14 @@ class DbActions {
       since: 'now',
       include_docs: true
     })
-    .on('change', change => {
-      if (change.deleted) {
-        self.postMessage({ event: 'deleted', method: 'changes', info: change })
-      } else { // updated/inserted
-        self.postMessage({ event: 'upsert', method: 'changes', info: change })
-      }
-    })
-    .on('error', err => self.postMessage({ event: 'error', method: 'changes', info: err }))
+      .on('change', change => {
+        if (change.deleted) {
+          self.postMessage({ event: 'deleted', method: 'changes', info: change })
+        } else { // updated/inserted
+          self.postMessage({ event: 'upsert', method: 'changes', info: change })
+        }
+      })
+      .on('error', err => self.postMessage({ event: 'error', method: 'changes', info: err }))
   }
 }
 
